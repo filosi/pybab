3c9e452fe6a98b8027b7453fae6c219a191c9b74
Pollo <pollo1_91@yahoo.it>
2012-12-01 16:31:56 +0100

    structure without django-restframeworks

---
+++ api/views/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ['upload_layer','delete_layer','upload_layer','delete_style',
+           'upload_indicator','delete_indicator',
+           'upload_statistical','delete_statistical',]
+
+from .layer_views import upload_layer,delete_layer,upload_layer,delete_style
+from .indicator_views import upload_indicator,delete_indicator
+from .statistical_views import upload_statistical,delete_statistical
---
+++ api/views/indicator_views.py
@@ -0,0 +1,5 @@
+def upload_indicator(request):
+    raise NotImplemented()
+
+def delete_indicator(request):
+    raise NotImplemented()
