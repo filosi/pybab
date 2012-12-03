07da738d31535861488f53df397bcd30bffdd7d3
Pollo <pollo1_91@yahoo.it>
2012-12-03 21:20:35 +0100

    registered both admin for cataloglayer using proxy model

--- api/admin.py
+++ api/admin.py
@@ -3,14 +3,19 @@ from pybab.api.models import UserStyle, CatalogShape
 from pybab.models import CatalogLayer
 from forms import ShapeForm, UserStyleForm
 
+class CatalogLayerAdmin(admin.ModelAdmin):
+    pass
+
 class CatalogShapeAdmin(admin.ModelAdmin):
     form = ShapeForm
     readonly_fields = ('gs_name',)
 
 class UserStyleAdmin(admin.ModelAdmin):
     form = UserStyleForm
+    fields = ('user',)
     readonly_fields = ('name',)
 
 admin.site.register(UserStyle, UserStyleAdmin)
+admin.site.register(CatalogLayer, CatalogLayerAdmin)
 admin.site.register(CatalogShape, CatalogShapeAdmin)
 
