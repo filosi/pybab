6251369dfaa831c22de7121ce5676c543ffe3d9e
Luca Colleoni <luca.colle92@gmail.com>
2012-12-07 12:59:32 +0100

    Adding post to statistical and indicator views

--- api/views/indicator_views.py
+++ api/views/indicator_views.py
@@ -2,9 +2,25 @@ from pybab.models import CatalogIndicator, IndicatorGroup
 from tojson import render_to_json
 from .commons import login_required_json_default, get_subtree_for
 
+from pybab.api.forms import UserIndicatorLinkForm
+from django.http import HttpResponseBadReqest
+
+
 @login_required_json_default
 @render_to_json()
 def catalog_indicator(request, tree_index):
     user = request.user
     if request.method == 'GET':
         return get_subtree_for(user, int(tree_index), IndicatorGroup, CatalogIndicator)
+    elif reques.method == 'POST':
+        indicator_form = UserIndicatorLinkForm(request.POST)
+    
+        if indicator_form.is_valid():
+            indicator_form.save()
+            return {'success': True}
+        else 
+            return {'success': False,
+                    'message': indicator_form.errors },
+                   { 'cls': HttpResponseBadRequest}   
+
+
--- api/views/statistical_views.py
+++ api/views/statistical_views.py
@@ -2,6 +2,9 @@ from pybab.models import CatalogStatistical, StatisticalGroup
 from tojson import render_to_json
 from .commons import login_required_json_default, get_subtree_for
 
+from pybab.api.forms import UserStatisticalLinkForm
+from django.http import HttpResponseBadReqest
+
 @login_required_json_default
 @render_to_json()
 def catalog_statistical(request, tree_index):
@@ -9,5 +12,15 @@ def catalog_statistical(request, tree_index):
     if request.method == 'GET':
         return get_subtree_for(user, int(tree_index), StatisticalGroup, CatalogStatistical)
     elif request.method == 'POST':
-        
+
+        statistical_form = UserStatisticalLinkForm(request.POST)
+
+        if statistical_form.is_valid():
+            statistical.save()
+            return {'success': True}
+        else
+            return {'success': False,
+                    'message': statistical_form.errors },
+                   { 'cls': HttpResponseBadRequest}   
+                     
 
