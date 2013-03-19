e4e527af3afec0bd0c837ac5b0c229ee91ff01bc
Roberto Bampi <bampi.roberto@gmail.com>
2013-03-19 18:34:01 +0100

    Setup should now work

--- setup.py
+++ setup.py
@@ -1,4 +1,33 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+  
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+django_dir = 'django'
+
+for dirpath, dirnames, filenames in os.walk(django_dir):
+# Ignore PEP 3147 cache dirs and those whose names start with '.'
+    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
 setup(
     name="pybab",
@@ -7,15 +36,16 @@ setup(
     url="https://github.com/MPBAUnofficial/pybab",
     author="Roberto Bampi",
     author_email="robampi@fbk.eu",
-    packages=["pybab"],
+    packages=packages,
+    data_files=data_files,
     classifiers=[
         'Programming Language :: Python',
         'License :: OSI Approved :: GPL License',
         'Operating System :: OS Indipendent',
         'Framework :: Django',
     ],
-    requires=["django"],
-    # Install hive and tojson by hand
-    #dependency_links=["https://github.com/MPBAUnofficial/hive/tarball/develop#egg=hive"]
+    install_requires=["django", "hive", "django-tojson"],
+    dependency_links=["https://github.com/MPBAUnofficial/hive/tarball/develop#egg=hive",
+                      "https://github.com/davidek/django-tojson/tarball/master#egg=django-tojson"]
 )
 
