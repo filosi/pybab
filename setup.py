f9dd9452b20381790aeaf6d9e44982dd958f9fed
Roberto Bampi <bampi.roberto@gmail.com>
2013-03-19 19:24:46 +0100

    Trying (again) to fix setup

--- setup.py
+++ setup.py
@@ -1,33 +1,4 @@
-import os
-from setuptools import setup
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-  
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-django_dir = 'django'
-
-for dirpath, dirnames, filenames in os.walk(django_dir):
-# Ignore PEP 3147 cache dirs and those whose names start with '.'
-    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+from setuptools import setup, find_packages
 
 setup(
     name="pybab",
@@ -36,8 +7,8 @@ setup(
     url="https://github.com/MPBAUnofficial/pybab",
     author="Roberto Bampi",
     author_email="robampi@fbk.eu",
-    packages=packages,
-    data_files=data_files,
+    packages=find_packages(),
+    include_package_data=True,
     classifiers=[
         'Programming Language :: Python',
         'License :: OSI Approved :: GPL License',
